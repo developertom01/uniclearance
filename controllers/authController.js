20c2f7364a599c8873f38f4e80f8ffcf6ee4877d
Thomas <sarpongthomas05@gmail.com>
2021-07-05 04:51:58 +0000

    added student's info to output

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,19 @@
 const Jwt  = require("jsonwebtoken")
 const config = require("../config")
+const {Student} =require("../models")
 
 
-const login = (req,res)=>{
+const login = async (req,res)=>{
 const token = Jwt.sign({
     exp: Math.floor(Date.now() / 1000) + 1000,
     sub:req.user.uid
 },config.appSecrete)
+const student = await Student.findOne({where:{userId:req.user.id}})
 res.json({
     status:"ok",
     token,
-    data:req.user
+    data:req.user,
+    student
 })
 } 
 
