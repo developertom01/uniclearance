5870ab70e57d02c69ff735fee91888b4ccac73b6
Thomas <sarpongthomas05@gmail.com>
2021-07-05 06:21:11 +0000

    added all permissions

---
+++ middleware/permissions/libraryAdmin.js
@@ -0,0 +1,7 @@
+const config = require("../../config")
+
+module.exports = (req,res,next)=>{
+    if(req.user.role !== config.userTypes.libraryAdmin)
+    return res.status(403).json("You do not have permission. Must be a library admin")
+    next()
+}
