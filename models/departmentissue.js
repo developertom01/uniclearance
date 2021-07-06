f3091a1beedc606d060cb17de5c05ae96128dc5c
Thomas <sarpongthomas05@gmail.com>
2021-07-06 23:48:16 +0000

    added department issues models and migrations

---
+++ models/departmentissue.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DepartmentIssue extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({Department,User}) {
+      this.belongsTo(User,{foreignKey:"userId",as:"user"})
+      this.belongsTo(Department,{foreignKey:"departmentId",as:"department"})
+    }
+  };
+  DepartmentIssue.init({
+    departmentId: {
+      type:DataTypes.INTEGER,
+      allowNull:false
+    },
+    uuid: {
+      type:DataTypes.UUID,
+      allowNull:false
+    },
+    userId: {
+      type:DataTypes.INTEGER,
+      allowNull:false
+    },
+    isCleared: {
+      type:DataTypes.BOOLEAN,
+      defaultValue:false
+    }
+  }, {
+    sequelize,
+    tableName:"departmentIssues",
+    modelName: 'DepartmentIssue',
+  });
+  return DepartmentIssue;
+};
